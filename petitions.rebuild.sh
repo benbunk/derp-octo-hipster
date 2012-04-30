9badc9f4e7b85b12ff647a434d0420266497d2fe
David Stoline <drupal@davidstoline.com>
2012-04-30 13:52:18 -0400

    ajasjkas

--- petitions.rebuild.sh
+++ petitions.rebuild.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Rebuilding Petitions..."
-#rm -Rf modules/contrib themes/ libraries
-drush -y make --contrib-destination=./sites/all/ build-petitions.make
+drush -y make build-petitions.make
